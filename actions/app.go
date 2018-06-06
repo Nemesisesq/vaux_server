0d90273a809af6d2d7ed501fa74beaf02f35b9d0
nemesisesq <nemesisesq@gmail.com>
2018-06-06 00:50:00 -0400

    got jwt token back from the app

--- actions/app.go
+++ actions/app.go
@@ -12,6 +12,8 @@ import (
 	"github.com/gobuffalo/packr"
 	"github.com/nemesisesq/vaux_server/models"
 	"github.com/nemesisesq/vaux_server/chat"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
 )
 
 // ENV is used to help switch settings based on where the
@@ -77,6 +79,32 @@ func App() *buffalo.App {
 func UserMiddleware() buffalo.MiddlewareFunc {
 	return func (next buffalo.Handler) buffalo.Handler {
 		return func(c buffalo.Context) error {
+
+			t := c.Request().Header.Get("Authorization")
+			//TODO Actualy verify the token and sign the secret properly refernced in https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-with-identity-providers.html
+			token, _ := jwt.Parse(t, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+				return []byte("secret"), nil
+			})
+			//TODO Check the token claims to make sure that they are valid
+			if claims, ok := token.Claims.(jwt.MapClaims); ok {
+
+				fmt.Println(claims)
+				//mapstructure.Decode(claims, &cognitoData)
+				//Rehydrate User
+
+				//user.Find(bson.M{"cognito_data.sub": cognitoData.Sub})
+				//user.CognitoData = cognitoData
+				//user.Upsert(bson.M{"user_info.sub": cognitoData.Sub})
+				//return user, nil
+				//json.NewEncoder(w).Encode(user)
+			} else {
+				//json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
+				//exp = Exception{Message: "Invalid authorization token"}
+				//return user, exp
+			}
 			return next(c)
 		}
 	}
